3880869d0e8613b372564bf0105133cfd5f1d898
senspond20 <senspond@gmail.com>
2021-02-07 22:33:17 +0900

    UploadController

--- src/main/java/com/sensweb/myapp/web/wysiwyg/controller/UploadController.java
+++ src/main/java/com/sensweb/myapp/web/wysiwyg/controller/UploadController.java
@@ -1,21 +1,23 @@
 package com.sensweb.myapp.web.wysiwyg.controller;
 
-import com.sensweb.myapp.common.model.FileInfoVO;
-import com.sensweb.myapp.common.model.FileIsSaveDto;
-import com.sensweb.myapp.common.utils.FileSaveUtils;
-import com.sensweb.myapp.common.utils.SystemAccessUtils;
-import com.sensweb.myapp.web.wysiwyg.service.UploadFileService;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import com.sensweb.myapp.common.model.FileSaveResponseDto;
+
+import com.sensweb.myapp.web.wysiwyg.service.FileService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import java.util.Map;
+
 
 @RestController
 // @RequestMapping("/api")
@@ -24,25 +26,36 @@ public class UploadController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    @Qualifier("UploadFileService")
-    private UploadFileService upService;
+    @Qualifier("fileService")
+    private FileService upService;
 
+    /*
+        파일 업로드
+    */
     @PostMapping("/uploadfile")
-    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile multipartFile){
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file){
+        FileSaveResponseDto fsd = upService.saveFile(file);
 
-        // 1. creating fileVO
-        FileIsSaveDto uploadInfo = upService.saveFile(multipartFile);
+        logger.debug("파일 저장 성공 여부 : {}", fsd.isSaveSuccess());
+        logger.debug("파일 정보 : {}", fsd.getFileInfo().toStringMultiline());
+        logger.debug("resourse {}", upService.load(fsd.getFileInfo().getRenameFileName()));    
+        return ResponseEntity.ok().body(fsd);
+    }
 
-        logger.debug("파일 저장 성공 여부 : {}", uploadInfo.isSaveSuccess());
-        logger.debug("파일 정보 : {}", uploadInfo.getFileInfo().toStringMultiline());
+    /*
+        모든 파일 삭제
+    */
+    @GetMapping("/deleteAllfiles")
+    public ResponseEntity<?> deleteAllfiles(){
+        upService.deleteAll();    
+        return ResponseEntity.ok().body(upService.loadAll());
+    }
 
-        // logger.debug(uploadInfo);
-
-        // 2. fileVO to file save servicer
-
-        // 3. success or fail ?
-        
-        // 4. return
-        return ResponseEntity.ok().body(uploadInfo);
+    /*
+        업로드한 파일 목록 
+    */
+    @GetMapping("/loadAllfiles")
+    public ResponseEntity<?> loadAllFiles(){
+        return ResponseEntity.ok().body(upService.loadAll());
     }
 }
